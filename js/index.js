c2e21c497c5419e92020304a3dbf3f074e7ed3a5
Luciano Ropero <lropero@gmail.com>
2015-02-21 04:17:35 +0000

    Build commit

--- js/index.js
+++ js/index.js
@@ -1,38 +1,21 @@
-var app = {
-	Views: {},
-	initialize: function() {
-		this.bindEvents();
-	},
-	bindEvents: function() {
-		document.addEventListener('deviceready', this.deviceReady, false);
-	},
-	deviceReady: function() {
-		FastClick.attach(document.body);
-		$('#content').html('ready');
-		// Backbone.history.start();
-		// this.trigger('started');
-	}
-};
+(function() {
+	'use strict';
 
-_.extend(app, Backbone.Events);
+	window.app = {
+		Views: {},
+		initialize: function() {
+			this.bindEvents();
+		},
+		bindEvents: function() {
+			document.addEventListener('deviceready', this.deviceReady, false);
+		},
+		deviceReady: function() {
+			$('#content').html('ready');
+			FastClick.attach(document.body);
+			Backbone.history.start();
+			this.trigger('started');
+		}
+	};
 
-// (function() {
-// 	'use strict';
-
-// 	window.app = {
-// 		Views: {},
-// 		init: function() {
-// 			this.bindEvents();
-// 		},
-// 		bindEvents: function() {
-// 			document.addEventListener('deviceready', this.deviceReady, false);
-// 		},
-// 		deviceReady: function() {
-// 			FastClick.attach(document.body);
-// 			Backbone.history.start();
-// 			this.trigger('started');
-// 		}
-// 	};
-
-// 	_.extend(app, Backbone.Events);
-// })();
+	_.extend(app, Backbone.Events);
+})();
