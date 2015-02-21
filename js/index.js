299e1f2f0e6fa0b21cf69696b4c8009b54c86bf2
Luciano Ropero <lropero@gmail.com>
2015-02-21 02:31:41 +0000

    Build commit

--- js/index.js
+++ js/index.js
@@ -14,12 +14,12 @@
 
 	_.extend(app, Backbone.Events);
 
-	window.init = function() {
-		document.addEventListener('deviceready', app.deviceReady, false);
-		// if(document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1) {
-		// 	document.addEventListener('deviceready', app.deviceReady, false);
-		// } else {
-		// 	app.deviceReady();
-		// }
-	}
+	// window.init = function() {
+	// 	document.addEventListener('deviceready', app.deviceReady, false);
+	// 	// if(document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1) {
+	// 	// 	document.addEventListener('deviceready', app.deviceReady, false);
+	// 	// } else {
+	// 	// 	app.deviceReady();
+	// 	// }
+	// }
 })();
