5441b2cf0b9c610432392ac86689dbe81c15e267
Luciano Ropero <lropero@gmail.com>
2015-02-26 18:05:22 +0000

    Build commit

--- js/_views/settings.js
+++ js/_views/settings.js
@@ -3,6 +3,9 @@
 
 	app.Views.settingsView = Backbone.View.extend({
 		el: '#settings',
+		events: {
+			'click li:not(.active)': 'switch'
+		},
 		initialize: function() {
 			var self = this;
 			$.get('js/_templates/settings.tpl', function(template) {
@@ -13,6 +16,11 @@
 		render: function() {
 			this.$el.html(this.template());
 			return this;
+		},
+		switch: function(e) {
+			this.$el.find('li.active').removeClass('active');
+			$(e.currentTarget).addClass('active');
+			var route = $(e.currentTarget).data('route');
 		}
 	});
 })();
