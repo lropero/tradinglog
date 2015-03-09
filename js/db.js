5a26e32a4dc7a2997fc736407f18918a393e0182
Luciano Ropero <lropero@gmail.com>
2015-03-09 15:06:15 +0000

    Build commit

--- js/db.js
+++ js/db.js
@@ -6,13 +6,13 @@
 	DatabaseController.prototype = {
 		init: function() {
 			if(typeof window.sqlitePlugin !== 'undefined') {
-				$('#main-stats-friends').empty().append('SQLITE<br />');
+				// $('#main-stats-friends').empty().append(typeof window.sqlitePlugin);
 				// var db = window.sqlitePlugin.openDatabase({
 				// 	name: 'TradingLog',
 				// 	location: 2
 				// });
 			} else {
-				$('#main-stats-friends').empty().append('WEB SQL<br />');
+				// $('#main-stats-friends').empty().append('WEB SQL<br />');
 				// var db = window.openDatabase('TradingLog', '1.0', 'TradingLog v1.0', 1024 * 1024);
 			}
 		}
