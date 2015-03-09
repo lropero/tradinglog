63b742671660ae1cf71c228f62923675ef819c87
Luciano Ropero <lropero@gmail.com>
2015-03-09 15:52:11 +0000

    Build commit

--- js/router.js
+++ js/router.js
@@ -1,4 +1,4 @@
-(function() {
+(function(app) {
 	'use strict';
 
 	var Router = Backbone.Router.extend({
@@ -71,4 +71,4 @@
 	};
 
 	window.navigationController = new NavigationController();
-})();
+})(app);
