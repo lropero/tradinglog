f88769fb22d80b8b77cd5679f39e57f5ac93acda
Luciano Ropero <lropero@gmail.com>
2015-03-16 08:07:11 -0300

    Build commit

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
 				app.databaseController.init(),
 				layout.deferred.promise()
 			).done(function() {
-				self.view = new app.Views.main();
+				self.view = new app.Views.friends();
 			});
 		},
 		loadView: function(view) {
