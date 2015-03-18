f19cca79a2f88dea34cf9c5ecaa2ce80a7a52f08
Luciano Ropero <lropero@gmail.com>
2015-03-18 11:19:08 -0300

    Build commit

--- js/misc/scroll.js
+++ js/misc/scroll.js
@@ -3,11 +3,12 @@
 
 	var Scroll = function(view) {
 		$(view.el).wrap('<div class="scroll"></div>');
-		view.maxScroll = $(view.el).height() - $('.scroll').height();
-		view.dragDown = snabbt(view.el, {
+		view.dragDown = snabbt($('.scroll'), {
 			manual: true,
 			position: [0, 30, 0]
 		});
+
+		view.maxScroll = $(view.el).height() - $('.scroll').height();
 
 		var hammer = new Hammer($('.scroll')[0]);
 		hammer.get('pan').set({
@@ -15,14 +16,15 @@
 		});
 
 		hammer.on('panstart', function(e) {
-			view.top = parseInt($(view.el).css('top'), 10);
+			view.posY = parseInt($(view.el).css('transform').split(',')[5], 10);
+			view.posY = isNaN(view.posY) ? 0 : view.posY;
 		});
 
 		hammer.on('pan', function(e) {
-			var delta = view.top + e.deltaY;
+			var delta = view.posY + e.deltaY;
 			if(delta > 0) {
 				delta = 0;
-				$(view.el).css('top', 0);
+				$(view.el).css('transform', 'translate(0, 0)');
 			} else if(Math.abs(delta) > view.maxScroll) {
 				delta = -view.maxScroll;
 			}
@@ -32,19 +34,19 @@
 						var value = e.distance > 200 ? 1 : $.easing.easeOutSine(1, e.distance, 0, 1, 200);
 						view.dragDown.setValue(value);
 					} else {
-						$(view.el).css('top', delta);
+						$(view.el).css('transform', 'translate(0, ' + delta + 'px)');
 					}
 					break;
 				case Hammer.DIRECTION_UP:
-					$(view.el).css('top', delta);
+					$(view.el).css('transform', 'translate(0, ' + delta + 'px)');
 					break;
 			};
 		});
 
 		hammer.on('panend', function(e) {
-			snabbt(view.el, {
+			snabbt($('.scroll'), {
 				callback: function() {
-					view.dragDown = snabbt(view.el, {
+					view.dragDown = snabbt($('.scroll'), {
 						manual: true,
 						position: [0, 30, 0]
 					});
