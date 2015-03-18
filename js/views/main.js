63f0a0c8cf818a854c22e3b7fad9d252eea0a5f5
Luciano Ropero <lropero@gmail.com>
2015-03-18 10:22:39 -0300

    Build commit

--- js/views/main.js
+++ js/views/main.js
@@ -21,7 +21,7 @@
 					trades: self.trades
 				}));
 				app.swiper.init('.active-swipe');
-				self.scroll = new app.Scroll(self);
+				// self.scroll = new app.Scroll(self);
 			});
 			return this;
 		},
