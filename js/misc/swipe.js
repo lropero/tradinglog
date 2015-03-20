1c4fd9442ab0e3259fe0b73fa1cd0fa516d763ee
Luciano Ropero <lropero@gmail.com>
2015-03-20 14:34:59 -0300

    Build commit

--- js/misc/swipe.js
+++ js/misc/swipe.js
@@ -10,37 +10,10 @@
 				startThreshold: [30, 20],
 				useCSSTranslation: false,
 
-				initiate: function() {
-					var $el = $(this.el);
-					if($el.hasClass('swiped')) {
-						$el.removeClass('swiped');
-					}
-					$.pep.restore();
+				rest: function() {
+					$('section#content').css('-webkit-overflow-scrolling', 'touch');
+					$('section#content').css('overflow-y', 'scroll');
 				},
-
-				// rest: function() {
-				// 	// console.log('rest');
-				// 	// // var $el = $(this.el);
-				// 	// // if($el.position().left > 0) {
-				// 	// // 	app.swipe.counter--;
-				// 	// // }
-				// 	// // if(app.swipe.counter === 0) {
-				// 	// 	console.log('si');
-				// 	this.coco = true;
-				// 	var self = this;
-				// 	$.each($('.active-swipe'), function() {
-				// 		if($(this).hasClass('swiped')) {
-				// 			self.coco = false;
-				// 			return;
-				// 		}
-				// 	});
-				// 	if(this.coco) {
-				// 		console.log('eee');
-				// 		$('section#content').css('-webkit-overflow-scrolling', 'touch');
-				// 		$('section#content').css('overflow-y', 'scroll');
-				// 	}
-				// 	// // }
-				// },
 
 				revertIf: function() {
 					var $el = $(this.el);
@@ -52,13 +25,13 @@
 				},
 
 				start: function() {
-					// console.log('no');
-					// // if(typeof app.swipe.counter === 'undefined') {
-					// // 	app.swipe.counter = 0;
-					// // }
-					// // app.swipe.counter++;
 					$('section#content').css('-webkit-overflow-scrolling', 'auto');
 					$('section#content').css('overflow-y', 'hidden');
+					var $el = $(this.el);
+					if($el.hasClass('swiped')) {
+						$el.removeClass('swiped');
+					}
+					$.pep.restore();
 				},
 
 				// stop: function(e) {
