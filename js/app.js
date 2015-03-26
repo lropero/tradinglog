61a6e60ccd26eff8aec905411dc9821ef5f05b38
Luciano Ropero <lropero@gmail.com>
2015-03-26 07:44:32 -0300

    Build commit

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@
 			/** A cache is used to hold the HTML rendered in app.Views.main so we
 			don't have to fetch all trades upon simple navigation. We then provide
 			a way to clear it triggering an event */
-			this.listenTo(app, 'clear', function(view) {
+			app.listenTo(app, 'clear', function(view) {
 				$('#content').html(view + '<br />');
 				$('#content').append(typeof app.cache + '<br />');
 				delete app.cache;
