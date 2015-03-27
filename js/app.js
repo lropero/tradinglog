7f4fc785c2bd593fee38ab948601f9b2ace49e5a
Luciano Ropero <lropero@gmail.com>
2015-03-27 09:55:44 -0300

    no message

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@
 			if(document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1) {
 				// cordova.plugins.Keyboard.disableScroll(true);
 				// cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-				FastClick.attach(document.body);
+				// FastClick.attach(document.body);
 			}
 		}
 	};
