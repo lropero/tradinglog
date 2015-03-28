cd8eb1cefb1d859e7b976d268b9e2bb7c35cdea3
Luciano Ropero <lropero@gmail.com>
2015-03-28 14:43:34 -0300

    no message

--- js/app.js
+++ js/app.js
@@ -34,11 +34,6 @@
 				app.view.deferred.done(function() {
 					if(navigator.splashscreen) {
 						navigator.splashscreen.hide();
-						window.addEventListener('native.keyboardhide', function() {
-							if($('div#isolate').is(':visible')) {
-								app.combine();
-							}
-						});
 					}
 				});
 
