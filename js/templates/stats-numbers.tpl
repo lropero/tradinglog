a522f07ed94ddec68457f3dad1262a0284bc551a
Luciano Ropero <lropero@gmail.com>
2015-04-20 18:30:12 -0300

    Doughnut

--- js/templates/stats-numbers.tpl
+++ js/templates/stats-numbers.tpl
@@ -1,3 +1,36 @@
 <script type="text/x-handlebars-template" id="stats-numbers-template">
-	STATS NUMBERS
+	<div class="header-box-swipe">
+		<div class="wrapper-calendar">
+			<div class="buttons-calendar">
+				<a href=""></a>
+				<a href=""></a>
+			</div>
+			<div class="calendar">April 2015</div>
+		</div>
+		<ul class="wrapper-radiobutton">
+			<li class="group-radiobutton">
+				<div class="radiobutton active"></div>
+				<span>All</span>
+			</li>
+			<li class="group-radiobutton">
+				<div class="radiobutton"></div>
+				<span>Long</span>
+			</li>
+			<li class="group-radiobutton">
+				<div class="radiobutton"></div>
+				<span>Short</span>
+			</li>
+		</ul>
+	</div>
+	<div class="wrapper-control-box-swipe">
+		<ul class="control-box-swipe end-left">
+			<li class="active"></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<div class="box-swipe">
+		<canvas id="doughnut" width="100" height="100"></canvas>
+		<div id="legend"></div>
+	</div>
 </script>
--- js/views/stats-numbers.js
+++ js/views/stats-numbers.js
@@ -15,6 +15,34 @@
 		render: function() {
 			app.trigger('change', 'stats-numbers');
 			this.$el.html(this.template());
+			var ctx = $('#doughnut').get(0).getContext('2d');
+			var data = [
+				{
+					value: 300,
+					color:"#ff3b30",
+					label: "Red"
+				},
+				{
+					value: 50,
+					color: "#4bd763",
+					label: "Green"
+				},
+				{
+					value: 100,
+					color: "#FDB45C",
+					label: "Yellow"
+				}
+			];
+			var options = {
+				animationEasing: 'easeOutElastic',
+				animationSteps: 50,
+				segmentStrokeColor: '#4020d0',
+				percentageInnerCutout: 60,
+				segmentStrokeWidth: 5,
+				legendTemplate : "<ul><% for(var i = 0; i < segments.length; i++) { %><li><div class=\"comm-how\"><%=segments[i].value%>%</div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+			};
+			var doughnut = new Chart(ctx).Doughnut(data, options);
+			// $('#legend').html(doughnut.generateLegend());
 			return this;
 		}
 	});
