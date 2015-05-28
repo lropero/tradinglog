f524486b603ce70287b67f5e30c4c818e142a76c
Luciano Ropero <lropero@gmail.com>
2015-05-28 01:55:45 -0300

    Grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,7 @@ module.exports = function(grunt) {
 					'js/vendor/alertify.js',
 					'js/vendor/backbone-sync.js',
 					'js/vendor/backbone-validation.js',
+					'js/vendor/big.js',
 					'js/vendor/Chart.js',
 					'js/vendor/handlebars-v3.0.1.js',
 					'js/vendor/jquery.cookie.js',
