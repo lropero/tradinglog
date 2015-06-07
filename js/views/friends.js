c892c1060193ec72e76e4847a571aed5a29af810
Luciano Ropero <lropero@gmail.com>
2015-06-07 13:19:31 -0300

    #WIP Twitter

--- js/views/friends.js
+++ js/views/friends.js
@@ -5,13 +5,24 @@
 		el: 'section#main-stats-friends',
 
 		initialize: function() {
+			if(typeof OAuth !== 'undefined') {
+				OAuth.initialize('oIPlwY07JghJFL8htlt8ZQ');
+			}
 			this.template = Handlebars.compile(app.templateLoader.get('friends'));
 			this.render();
 		},
 
 		render: function() {
+			var self = this;
 			app.trigger('change', 'friends');
 			this.$el.html(this.template());
+
+			if(typeof OAuth !== 'undefined') {
+				OAuth.popup('twitter').done(function(result) {
+					self.$el.html(result);
+				});
+			}
+
 			return this;
 		}
 	});
